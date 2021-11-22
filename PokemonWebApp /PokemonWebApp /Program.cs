6c578172daad47c53c9ca91f21a7a916382558fb
Wallace Leonel <69112012+wallaceleonel@users.noreply.github.com>
2021-11-21 22:12:16 -0300

    Update Program.cs

--- PokemonWebApp /PokemonWebApp /Program.cs
+++ PokemonWebApp /PokemonWebApp /Program.cs
@@ -21,6 +21,7 @@ namespace PokemonWebApp
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
+                                          
                 {
                     webBuilder.UseStartup<Startup>();
                 });
